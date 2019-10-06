b3b91ddfa5451153344d9ee14990c69d949ab35a
Andrew Smith <smith.andrew.l@gmail.com>
2019-10-06 16:56:16 -0500

    Added more type hints.

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import math
 import os
 import sys
+from typing import Tuple
 
 import pygame
 
@@ -10,31 +11,33 @@ from pygame.locals import KEYDOWN
 
 from assets import *
 from config import *
-from game_data import GameData
+from game_data import GameData, GameBoard
 from graphics import GameRenderer
 
-rows = 6
-cols = 7
-sq_size = 100
-width = cols * sq_size
-height = (rows + 1) * sq_size
-size = (width, height)
-radius = int(sq_size / 2 - 5)
+rows: int = 6
+cols: int = 7
+sq_size: int = 100
+width: int = cols * sq_size
+height: int = (rows + 1) * sq_size
+size: Tuple[int, int] = (width, height)
+radius: int = int(sq_size / 2 - 5)
 
 pygame.init()
 screen = pygame.display.set_mode(size)
+
 pygame.display.set_caption("Connect Four | Mayank Singh")
+
 myfont = pygame.font.SysFont("monospace", 75)
 label = myfont.render("CONNECT FOUR!!", 1, white)
 
-game_data = GameData()
-renderer = GameRenderer(screen)
+game_data: GameData = GameData()
+renderer: GameRenderer = GameRenderer(screen)
 
 screen.blit(label, (40, 10))
 pygame.display.update()
 
 
-def undo_move(board, row, col):
+def undo_move(board: GameBoard, row: int, col: int):
     game_data.game_board.drop_piece(row, col, 0)
     filled_circle(screen, int(row), int(col), radius, black)
     aacircle(screen, int(row), int(col), radius, black)
@@ -53,7 +56,7 @@ while not game_data.game_over:
             sys.exit()
         if event.type == pygame.MOUSEMOTION:
             pygame.draw.rect(screen, black, (0, 0, width, sq_size))
-            posx = event.pos[0]
+            posx: int = event.pos[0]
 
             if game_data.turn == 0:
                 renderer.draw_red_coin(posx - (sq_size / 2), int(sq_size) - sq_size + 5)
@@ -64,13 +67,17 @@ while not game_data.game_over:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, black, (0, 0, width, sq_size))
             if game_data.turn == 0:
-                posx = event.pos[0]
-                col = int(math.floor(posx / sq_size))
+                posx: int = event.pos[0]
+
+                col: int = int(math.floor(posx / sq_size))
+
                 if game_data.game_board.is_valid_location(col):
-                    row = game_data.game_board.get_next_open_row( col)
+                    row: int = game_data.game_board.get_next_open_row( col)
+
                     game_data.last_move_row = row
                     game_data.last_move_col = col
-                    game_data.game_board.drop_piece( row, col, 1)
+                    game_data.game_board.drop_piece(row, col, 1)
+
                 game_data.game_board.print_board()
 
                 if game_data.game_board.winning_move( 1):
@@ -81,13 +88,16 @@ while not game_data.game_over:
                     game_data.game_over = True
                 pygame.display.update()
             else:
-                posx = event.pos[0]
-                col = int(math.floor(posx / sq_size))
+                posx: int = event.pos[0]
+                col: int = int(math.floor(posx / sq_size))
+
                 if game_data.game_board.is_valid_location(col):
-                    row = game_data.game_board.get_next_open_row( col)
+                    row: int = game_data.game_board.get_next_open_row( col)
+
                     game_data.last_move_row = row
                     game_data.last_move_col = col
                     game_data.game_board.drop_piece( row, col, 2)
+
                 game_data.game_board.print_board()
 
                 if game_data.game_board.winning_move( 2):
@@ -102,9 +112,9 @@ while not game_data.game_over:
 
         if event.type == KEYDOWN:
             if event.key == pygame.K_z:
-                mods = pygame.key.get_mods()
+                mods: int = pygame.key.get_mods()
                 if mods & pygame.KMOD_CTRL:
-                    undo_move(game_data.game_board.board, game_data.last_move_row, game_data.last_move_col)
+                    undo_move(game_data.game_board, game_data.last_move_row, game_data.last_move_col)
                     game_data.turn += 1
                     game_data.turn = game_data.turn % 2
         # tie
