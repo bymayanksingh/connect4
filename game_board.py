67d3495698d34e66d222c42f850804f2d4b6342e
Andrew Smith <smith.andrew.l@gmail.com>
2019-10-20 11:23:28 -0500

    Refactored and simplified the logic to check for a winning game board.

--- game_board.py
+++ game_board.py
@@ -32,48 +32,97 @@ class GameBoard:
             if self.board[row][col] == 0:
                 return row
 
+    def check_square(self, piece, r, c):
+        """
+        Checks if a particular square is a certain color.  If
+        the space is off of the board it returns False.
+
+        :param piece: The piece color to look for.
+        :param r: The row to check.
+        :param c: The column to check.
+        :return: Whether the square is on the board and has the color/piece specified.
+        """
+        if r < 0 or r >= self.rows:
+            return False
+
+        if c < 0 or c >= self.cols:
+            return False
+
+        return self.board[r][c] == piece
+
+    def horizontal_win(self, piece, r,c):
+        """
+        Checks if there is a horizontal win at the position (r,c)
+        :param piece: The color of the chip to check for.
+        :param r: The row.
+        :param c: The column.
+        :return: Whether there is a horizontal win at the position (r, c).
+        """
+        return (
+                self.check_square(piece, r, c)
+                and self.check_square(piece, r, c + 1)
+                and self.check_square(piece, r, c + 2)
+                and self.check_square(piece, r, c + 3)
+        )
+
+    def vertical_win(self, piece, r, c):
+        """
+        Checks if there is vertical win at the position (r, c)
+        :param piece: The color of the chip to check for.
+        :param r: The row
+        :param c: The column
+        :return: Whether there is a vertical win at the position (r, c)
+        """
+        return (
+            self.check_square(piece, r, c)
+            and self.check_square(piece, r + 1, c)
+            and self.check_square(piece, r + 2, c)
+            and self.check_square(piece, r + 3, c)
+        )
+    def diagonal_win(self, piece, r, c):
+        """
+        Checks if there is a diagonal_win at the position (r, c)
+        :param piece: The color of the chip to check for.
+        :param r: The row
+        :param c: The column
+        :return: Whether there is a diagonal win at the position (r,c)
+        """
+        return (
+            self.check_square(piece, r, c)
+            and  self.check_square(piece, r + 1, c + 1)
+            and  self.check_square(piece, r + 2, c + 2)
+            and  self.check_square(piece, r + 3, c + 3)
+        ) or (
+            self.check_square(piece, r, c)
+            and self.check_square(piece, r - 1, c + 1)
+            and self.check_square(piece, r - 2, c + 2)
+            and self.check_square(piece, r - 3, c + 3)
+        )
+
+
     def winning_move(self, piece):
-        for c in range(self.cols - 3):
+        """
+        Checks if the current piece has won the game.
+        :param piece: The color of the chip to check for.
+        :return: Whether the current piece has won the game.
+        """
+        for c in range(self.cols):
             for r in range(self.rows):
                 if (
-                        self.board[r][c] == piece
-                        and self.board[r][c + 1] == piece
-                        and self.board[r][c + 2] == piece
-                        and self.board[r][c + 3] == piece
+                    self.horizontal_win(piece, r, c)
+                    or self.vertical_win(piece, r, c)
+                    or self.diagonal_win(piece, r, c)
+                    or self.diagonal_win(piece, r, c)
                 ):
                     return True
+        return False
 
-        for c in range(self.cols):
-            for r in range(self.rows - 3):
-                if (
-                        self.board[r][c] == piece
-                        and self.board[r + 1][c] == piece
-                        and self.board[r + 2][c] == piece
-                        and self.board[r + 3][c] == piece
-                ):
-                    return True
-
-        for c in range(self.cols - 3):
-            for r in range(self.rows - 3):
-                if (
-                        self.board[r][c] == piece
-                        and self.board[r + 1][c + 1] == piece
-                        and self.board[r + 2][c + 2] == piece
-                        and self.board[r + 3][c + 3] == piece
-                ):
-                    return True
-
-        for c in range(self.cols - 3):
-            for r in range(3, self.rows):
-                if (
-                        self.board[r][c] == piece
-                        and self.board[r - 1][c + 1] == piece
-                        and self.board[r - 2][c + 2] == piece
-                        and self.board[r - 3][c + 3] == piece
-                ):
-                    return True
 
     def tie_move(self):
+        """
+        Checks for a tie game.
+        :return:  Whether a tie has occurred.
+        """
         slots_filled: int = 0
 
         for c in range(self.cols):
