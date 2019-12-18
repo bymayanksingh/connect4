352bcaff3a029998ee831ad89cf493899879eb1c
KomalKalyan <komal.dbz@gmail.com>
2019-12-19 01:40:39 +0530

    Updated and Interactive

--- connect_game.py
+++ connect_game.py
@@ -102,7 +102,6 @@ class ConnectGame:
         if self.game_data.game_over:
             print(os.getpid())
             pygame.time.wait(1000)
-            os.system("game.py")
 
     def draw(self):
         """
