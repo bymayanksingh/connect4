6e14b5da1b888e70fdb844fe478c42deda5e072a
Pablo <pablete.arino@gmail.com>
2021-07-22 13:27:33 +0200

    main function added to improve performance

--- game.py
+++ game.py
@@ -100,53 +100,58 @@ def text_objects(text, font, color):
     return text_surface, text_surface.get_rect()
 
 
-def message_display(text, color, p, q, v):
+def message_display(text, color, p, q, v, screen):
     large_text = pygame.font.SysFont("monospace", v)
     text_surf, text_rect = text_objects(text, large_text, color)
     text_rect.center = (p, q)
     screen.blit(text_surf, text_rect)
 
 
-pygame.init()
-screen = pygame.display.set_mode(GameData().size)
-pygame.display.set_caption("Connect Four | Mayank Singh")
-message_display("CONNECT FOUR!!", white, 350, 150, 75)
-message_display("HAVE FUN!", (23, 196, 243), 350, 300, 75)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(GameData().size)
+    pygame.display.set_caption("Connect Four | Mayank Singh")
+    message_display("CONNECT FOUR!!", white, 350, 150, 75, screen)
+    message_display("HAVE FUN!", (23, 196, 243), 350, 300, 75, screen)
 
-running = True
-while running:
+    running = True
+    while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    def button(msg, x, y, w, h, ic, ac, action=None):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
+        def button(msg, x, y, w, h, ic, ac, action=None):
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
 
-        if x + w > mouse[0] > x and y + h > mouse[1] > y:
-            pygame.draw.rect(screen, ac, (x, y, w, h))
+            if x + w > mouse[0] > x and y + h > mouse[1] > y:
+                pygame.draw.rect(screen, ac, (x, y, w, h))
 
-            if click[0] == 1 and action is not None:
-                action()
-        else:
-            pygame.draw.rect(screen, ic, (x, y, w, h))
+                if click[0] == 1 and action is not None:
+                    action()
+            else:
+                pygame.draw.rect(screen, ic, (x, y, w, h))
 
-        small_text = pygame.font.SysFont("monospace", 30)
-        text_surf, text_rect = text_objects(msg, small_text, white)
-        text_rect.center = ((x + (w / 2)), (y + (h / 2)))
-        screen.blit(text_surf, text_rect)
+            small_text = pygame.font.SysFont("monospace", 30)
+            text_surf, text_rect = text_objects(msg, small_text, white)
+            text_rect.center = ((x + (w / 2)), (y + (h / 2)))
+            screen.blit(text_surf, text_rect)
 
-    button("2 PLAYERS", 125, 450, 170, 50, white, white, start_player_vs_player)
-    button("2 PLAYERS", 127, 452, 166, 46, black, black, start_player_vs_player)
+        button("2 PLAYERS", 125, 450, 170, 50, white, white, start_player_vs_player)
+        button("2 PLAYERS", 127, 452, 166, 46, black, black, start_player_vs_player)
 
-    # button("AI", 300, 450, 100, 50, white, white, start_player_vs_ai)
-    # button("AI", 302, 452, 96, 46, black, black, start_player_vs_ai)
+        # button("AI", 300, 450, 100, 50, white, white, start_player_vs_ai)
+        # button("AI", 302, 452, 96, 46, black, black, start_player_vs_ai)
 
-    button("COMPUTER", 125, 510, 170, 50, white, white, start_player_vs_ai)
-    button("AI", 127, 512, 166, 46, black, black, start_player_vs_ai)
+        button("COMPUTER", 125, 510, 170, 50, white, white, start_player_vs_ai)
+        button("AI", 127, 512, 166, 46, black, black, start_player_vs_ai)
 
-    button("QUIT", 500, 450, 100, 50, white, white, quit)
-    button("QUIT", 502, 452, 96, 46, black, black, quit)
+        button("QUIT", 500, 450, 100, 50, white, white, quit)
+        button("QUIT", 502, 452, 96, 46, black, black, quit)
 
-    pygame.display.update()
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
