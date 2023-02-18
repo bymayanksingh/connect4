10e73b1ee51e5701b18ce56793e6cbce47e3591e
Mayank Singh <mayank.singh081997@gmail.com>
2023-02-19 04:39:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-# Colors
-yellow = (255, 255, 0)
-red = (255, 0, 0)
-blue = (0, 0, 255)
-white = (255, 255, 255)
-black = (0, 0, 0)
+"""A module that defines color constants."""
+
+# Define color constants as tuples of three integers (RGB values).
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
